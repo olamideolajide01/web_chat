2f4abb713c4ac7fa28db0f6bca98bbe43a2cdf43
olajide olamide <olajideseyi8181@gmail.com>
2022-10-14 14:23:08 +0100

    installed tailwind css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Message from "./pages/Message";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <p>my new page</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/message" element={<Message />} />
+      </Routes>
+    </Router>
   );
 }
 
