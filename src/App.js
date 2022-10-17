e9872162f27612fd68b22af120070b63d72ad5f5
Olamide Olajide <82158924+olamideolajide01@users.noreply.github.com>
2022-10-17 16:37:30 +0100

    Revert "Olammy"

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
-import Navbar from "./components/Navbar";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Errorpage from "./pages/Error";
-import { useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const { curUser } = useContext(AuthContext);
-
-  const ProtectedRoute = ({ children }) => {
-    if (!curUser) {
-      return <Navigate to={"/login"} />;
-    } else return children;
-  };
-
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<Errorpage />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <p>my new page</p>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import AuthContextProvider from "./context/AuthContext";
-import ChatContextProvider from "./context/ChatContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AuthContextProvider>
-    <ChatContextProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ChatContextProvider>
-  </AuthContextProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
