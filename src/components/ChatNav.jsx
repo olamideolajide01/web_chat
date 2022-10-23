35d939631ee38f84170cd34b3c632aa1d7e7491a
olajide olamide <olajideseyi8181@gmail.com>
2022-10-23 23:00:18 +0100

    added menu icon and updated message chat

--- src/components/ChatNav.jsx
+++ src/components/ChatNav.jsx
@@ -6,7 +6,7 @@ export default function ChatNav() {
   return (
     <div className="flex  justify-between px-2 py-2 h-[40px] bg-slate-700 ">
       <span>
-        <img src="/menu.svg" alt="" />
+        <img src="/menu.svg" alt="" className="sm:hidden" />
       </span>
       <span className="text-white font-thin">{data.user?.displayName}</span>
       <div className="flex gap-2">
